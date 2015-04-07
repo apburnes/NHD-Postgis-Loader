4b0bf429bba99332d5ecc9cc4bd8d7adfed74f7a
Andrew Burnes <apburnes@gmail.com>
2015-04-06 20:41:01 -0600

    Added hydro unit transformations

--- sql/createSchema.sql
+++ sql/createSchema.sql
@@ -2,23 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "postgis";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Tables and geometry from the NHD file.gdb upload using ogr2ogr
-
-  -- ogc_fid serial NOT NULL,
-  -- wkb_geometry geometry(MultiLineString,4269),
-  -- permanent_identifier character varying,
-  -- fdate timestamp with time zone,
-  -- resolution integer,
-  -- gnis_id character varying,
-  -- gnis_name character varying,
-  -- lengthkm double precision,
-  -- reachcode character varying,
-  -- flowdir integer,
-  -- wbarea_permanent_identifier charVARCHARr varying,
-  -- ftype integer,
-  -- fcode integer,
-  -- shape_length double precision,
-  -- enabled integer,
-  -- CONSTRAINT nhdflowline_pkey PRIMARY KEY (ogc_fid)
+-- Table: nhdflowlines
 
 CREATE TABLE IF NOT EXISTS flowlines (
   id uuid DEFAULT uuid_generate_v4() NOT NULL,
@@ -37,3 +21,50 @@ CREATE TABLE IF NOT EXISTS flowlines (
   enabled INT,
   the_geom geometry(MultiLineString, 900913)
 );
+
+CREATE INDEX flowlines_geom_idx ON flowlines USING gist (the_geom);
+
+-- Table: All wbdhuc polygons
+CREATE TABLE IF NOT EXISTS hydro_unit_boundaries (
+  id uuid DEFAULT uuid_generate_v4() NOT NULL,
+  tnm_id VARCHAR,
+  meta_src_id VARCHAR,
+  src_data_desc VARCHAR,
+  src_originator VARCHAR,
+  src_feature_id VARCHAR,
+  load_date TIMESTAMP WITH TIME ZONE,
+  gnis_id INT,
+  area_acres DOUBLE PRECISION,
+  area_sqkm DOUBLE PRECISION,
+  states VARCHAR,
+  hu_code VARCHAR(12),
+  hu_class TEXT,
+  name VARCHAR,
+  hu_type VARCHAR,
+  hu_mod VARCHAR,
+  to_huc VARCHAR(12),
+  non_contrib_acres DOUBLE PRECISION,
+  non_contrib_sqkm DOUBLE PRECISION,
+  the_geom geometry(MultiPolygon, 900913),
+  CONSTRAINT hydro_unit_boundaries_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX hydro_unit_geom_idx ON hydro_unit_boundaries USING gist (the_geom);
+
+-- Function: classify wdbhuc
+CREATE OR REPLACE FUNCTION hu_classify(huc varchar)
+  RETURNS text AS
+$$
+  BEGIN
+    RETURN CASE
+      WHEN char_length(huc) = 2 THEN 'Region'
+      WHEN char_length(huc) = 4 THEN 'Subregion'
+      WHEN char_length(huc) = 6 THEN 'Basin'
+      WHEN char_length(huc) = 8 THEN 'Subbasin'
+      WHEN char_length(huc) = 10 THEN 'Watershed'
+      WHEN char_length(huc) >= 12 THEN 'Subwatershed'
+      ELSE NULL
+    END;
+  END;
+$$
+LANGUAGE 'plpgsql';
